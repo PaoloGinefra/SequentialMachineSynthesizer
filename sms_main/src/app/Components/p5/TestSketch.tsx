60340b7bf202220b1d95e7e82858ec818f1cd2eb
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-02-01 10:07:09 +0100

    chore: updated p5 test sketch to handle state

--- sms_main/src/app/Components/p5/TestSketch.tsx
+++ sms_main/src/app/Components/p5/TestSketch.tsx
@@ -1,22 +1,46 @@
-import React from "react";
-import { type Sketch } from "@p5-wrapper/react";
+import { P5CanvasInstance, SketchProps } from "@p5-wrapper/react";
 import { NextReactP5Wrapper } from "@p5-wrapper/next";
+import { useEffect, useState } from "react";
 
-const sketch: Sketch = (p5) => {
-    p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
-
-    p5.draw = () => {
-        p5.background(250);
-        p5.normalMaterial();
-        p5.push();
-        p5.rotateZ(p5.frameCount * 0.01);
-        p5.rotateX(p5.frameCount * 0.01);
-        p5.rotateY(p5.frameCount * 0.01);
-        p5.plane(100);
-        p5.pop();
-    };
+type MySketchProps = SketchProps & {
+    rotation: number;
 };
 
+function sketch(p5: P5CanvasInstance<MySketchProps>) {
+    let rotation = 0;
+
+    p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
+
+    p5.updateWithProps = (props: MySketchProps) => {
+        if (props.rotation) {
+            rotation = (props.rotation * Math.PI) / 180;
+        }
+    };
+
+    p5.draw = () => {
+        p5.background(100);
+        p5.normalMaterial();
+        p5.noStroke();
+        p5.push();
+        p5.rotateY(rotation);
+        p5.box(100);
+        p5.pop();
+    };
+}
+
 export default function TestSketch() {
-    return <NextReactP5Wrapper sketch={sketch} />;
+    const [rotation, setRotation] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(
+            () => setRotation(rotation => rotation + 100),
+            100
+        );
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    return <NextReactP5Wrapper sketch={sketch} rotation={rotation} />;
 }
